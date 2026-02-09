7ec1d8372b819a8b8d49df6eded023688c7ecfab
zbeidas26 <zbeidas@uw.edu>
2026-02-09 20:25:46 +0000

    added week 5 file

---
+++ week5.py
@@ -0,0 +1,13 @@
+Animals = ["Dog", "Cat", "Turtle", "Horse", "Seal", "Tiger"]
+print(Animals)
+print("The first two items in the list are:", Animals[0], ",", Animals[1])
+print("The middle two items in the list are:", Animals[2], ",", Animals[3])
+print("The first and last items in the list are:", Animals[0], ",", Animals[-1])
+
+menu = ("Pizza", "Burger", "Pasta", "Salad", "Soup")
+for food in menu:
+    print(food)
+    
+updated_menu = ("Pizza", "Tacos", "Pasta", "Salad", "Ramen")
+for food in updated_menu:
+    print(food)
